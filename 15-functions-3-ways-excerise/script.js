dcf7c9a37ea82c77e30ac4539f4ec2fe516950aa
JoniBezemer <77683711+JoniBezemer@users.noreply.github.com>
2021-05-11 15:26:13 +0200

    Add files via upload

---
+++ 15-functions-3-ways-excerise/script.js
@@ -0,0 +1,25 @@
+//function expression
+
+const functionExpression = function (numberA, numberB){
+    return(numberA + numberB)
+};
+
+console.log(functionExpression(3, 6))
+
+// function declaration
+
+function functionDeclaration(number1, number2) {
+    return number1 + number2;
+  }
+  
+  console.log(functionDeclaration(3, 6)); 
+
+//arrow function
+
+const functionArrow = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(functionArrow(3, 6));
+
+  
